2f3aef3aaf39bed19ef2c41ab52adba6fdb3b7bf
Neostis <108649813+Neostis@users.noreply.github.com>
2023-12-06 18:00:31 +0700

    update: upload file, get a singel file

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 app.use("/files", express.static("files"));
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+const { ObjectId } = require("mongodb");
 
 // MongoDB connection
 const mongoUrl =
@@ -15,6 +16,7 @@ const mongoUrl =
 mongoose
   .connect(mongoUrl, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to database");
@@ -24,61 +26,81 @@ mongoose
 // Multer
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./files");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null, uniqueSuffix + file.originalname);
-  },
-});
+const storage = multer.memoryStorage(); // Use memory storage for multer
 
-// Import and use the model
-require("./filesDetails");
-const FilesDetails = mongoose.model("FilesDetails");
 const upload = multer({ storage: storage });
 
 app.post("/upload-files", upload.single("file"), async (req, res) => {
   const title = req.file.originalname;
-  // Use lastIndexOf to find the last dot in the string
-  const lastDotIndex = title.lastIndexOf(".");
-
-  // Check if there is a dot in the string
-  const filenameWithoutExtension =
-    lastDotIndex !== -1 ? title.substring(0, lastDotIndex) : title;
-  const type = /[.]/.exec(title) ? /[^.]+$/.exec(title) : undefined;
+  const type = path.extname(title).substr(1);
   const lastModified = Date.now();
+
   try {
-    await FilesDetails.create({
-      title: filenameWithoutExtension,
-      type: type[0],
-      lastModified: lastModified,
+    const db = mongoose.connection.db; // Access the native MongoDB driver's database object
+    const bucket = new mongoose.mongo.GridFSBucket(db);
+
+    // Create a readable stream from the file buffer
+    const readableStream = require('stream').Readable.from([req.file.buffer]);
+
+    // Upload the file to GridFS
+    const uploadStream = bucket.openUploadStream(title, {
+      contentType: req.file.mimetype,
+      metadata: {
+        title: title,
+        type: type,
+        lastModified: lastModified,
+      },
     });
 
-    const filePath = path.join(__dirname, 'files', req.file.filename);
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error('Error deleting file:', err);
-      }
-    });
+    readableStream.pipe(uploadStream);
 
-    res.send({ status: "ok" });
+    uploadStream.on('finish', async () => {
+      // Remove the file from the local filesystem
+      // (assuming that you don't need it after uploading to GridFS)
+      res.send({ status: "ok" });
+    });
   } catch (error) {
+    console.error(error);
     res.json({ status: error });
   }
 });
 
 app.get("/get-files", async (req, res) => {
+  const db = mongoose.connection.db; // Access the native MongoDB driver's database object
+  const filesCollection = db.collection('fs.files');
+
   try {
-    FilesDetails.find({}).then((data) => {
-      res.send({ status: "ok", data: data });
-    });
+    // Retrieve all files from fs.files
+    const allFiles = await filesCollection.find().toArray();
+
+    // Send the list of files as a response
+    res.json(allFiles);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ status: "Internal Server Error" });
+    res.status(500).send('Internal Server Error');
   }
 });
+
+app.get('/get-file/:fileId', async (req, res) => {
+  try {
+    const fileId = req.params.fileId;
+
+    const db = mongoose.connection.db; // Access the native MongoDB driver's database object
+    const bucket = new mongoose.mongo.GridFSBucket(db);
+
+    const fileMetadata = await bucket.find({ _id: ObjectId(fileId) }).toArray();
+
+    if (fileMetadata.length === 0) {
+      return res.status(404).json({ error: "File does not exist." });
+    }
+
+    bucket.openDownloadStream(ObjectId(fileId)).pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 
 // APIs
 app.get("/", async (req, res) => {
