f689df47b1fb79ffc8650daabdad41bf427aaf9c
Neostis <108649813+Neostis@users.noreply.github.com>
2023-12-18 15:27:04 +0700

    update: addMember

--- memberModel.js
+++ memberModel.js
@@ -1,28 +1,16 @@
 const mongoose = require('mongoose');
 const { ObjectId } = require("mongodb");
 
-const projectSchema = new mongoose.Schema(
-  {
-    projectId: String,
-    type: String,
-  },
-  { _id: false }
-);
-
 const memberSchema = new mongoose.Schema({
   name: String,
   role: String,
   email: String,
-<<<<<<< HEAD
   projects: [
     {
       projectID: { type: ObjectId, required: true },
       type: { type: String, required: true },
     },
  ],
-=======
-  projects: [projectSchema],
->>>>>>> 794e421d29d7e11a58363191e05bee321a9db3de
 });
 
 const Member = mongoose.model('TestMembers', memberSchema); // Specify 'TestMembers' as the collection name
