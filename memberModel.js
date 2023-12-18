cad0bb08c86fc09d1721079277734ee15c491563
Jarpip <naluedon.a@gmail.com>
2023-12-18 18:47:12 +0700

    Fix Model

--- memberModel.js
+++ memberModel.js
@@ -7,12 +7,12 @@ const memberSchema = new mongoose.Schema({
   email: String,
   projects: [
     {
-      projectID: { type: ObjectId, required: true },
+      projectId: { type: ObjectId, required: true },
       type: { type: String, required: true },
       _id: false,
     },
   ],
 });
 
-const Member = mongoose.model("Members", memberSchema); // Specify 'TestMembers' as the collection name
+const Member = mongoose.model("Member", memberSchema, "Members"); // Specify 'TestMembers' as the collection name
 module.exports = Member;
