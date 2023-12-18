838245808ff7631940b16dbe6338746b8d271b31
Neostis <108649813+Neostis@users.noreply.github.com>
2023-12-18 15:39:25 +0700

    update: change schema to Members

--- memberModel.js
+++ memberModel.js
@@ -14,5 +14,5 @@ const memberSchema = new mongoose.Schema({
   ],
 });
 
-const Member = mongoose.model("TestMembers", memberSchema); // Specify 'TestMembers' as the collection name
+const Member = mongoose.model("Members", memberSchema); // Specify 'TestMembers' as the collection name
 module.exports = Member;
