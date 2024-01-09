73281a67df073d628548316995927266423d633d
Jarpip <naluedon.a@gmail.com>
2024-01-09 08:58:29 +0700

    remove duplicate import

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(cors());
 
 const bodyParser = require("body-parser");
 const Project = require("./projectModel");
-const Timeline = require("./timelineModel");
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
